6cec4afcac7f8d620b1aa4c83bcaa91b9f98902f
= <=>
2020-07-24 09:37:43 -0300

    Try Catch block added to the insert function / getUsers() and getUserByEmail added

--- utils/database.js
+++ utils/database.js
@@ -31,28 +31,55 @@ class Database {
             * User: name, email, password
             * ToDo: title, description
         */
-
         const keys = Object.keys(object)
 
         const addUserKeys = ['name', 'email', 'password']
         const addTodoKeys = ['title', 'description']
 
-        if (keys.every((key, index) => key === addUserKeys[index])) {
+        try {
 
-            const { name, email, password } = object
-            await this.users.insertOne({ name, email, password })
+            /*
+                * insert() is used by both addUser and addTodo.
+                * It then has to check which one it is.
+                * Once checked, it executes the proper function.
+            */
 
-            return true
+            if (keys.every((key, index) => key === addUserKeys[index])) {
+
+                const { name, email, password } = object
+                await this.users.insertOne({ name, email, password })
+
+                return true
+            }
+
+            if (keys.every((key, index) => key === addTodoKeys[index])) {
+
+                const { title, description } = object
+                await this.todos.insertOne({ title, description })
+
+                return true
+            }
+        } catch (error) {
+            throw error
         }
+    }
 
-        if (keys.every((key, index) => key === addTodoKeys[index])) {
-
-            const { title, description } = object
-            await this.todos.insertOne({ title, description })
-
-            return true
+    async getUsers() {
+        try {
+            const users = await this.users.findOne({})
+            return users
+        } catch (error) {
+            throw error
         }
+    }
 
+    async getUserByEmail(email = '') {
+        try {
+            const user = await this.users.findOne({ email })
+            return user
+        } catch (error) {
+            throw error
+        }
     }
 }
 
