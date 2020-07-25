e3bc2efc7d426b48284d82bcf5beef47f992fc5b
= <=>
2020-07-25 17:08:32 -0300

    Added a todo tree / Starting to refactor the whole code / Few implementations to come

--- routes/incorrect-credentials.js
+++ routes/incorrect-credentials.js
@@ -1,10 +1,8 @@
 import express from 'express'
 const router = express.Router()
 
-
 router.get('/', (req, res) => {
     res.render('incorrect-credentials.ejs')
 })
-
 
 export default router
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 /*
-    * TODO: Start thinking about the css
-    * TODO: Start working on the todo part
+    * TODO: Work on the css from the site
+    * TODO: Check for every entry of the user data
+        * every time a database is called
+        * every time data is retrieved by the user
+        * do not trust any data that comes or goes to the user
+        * check everything
+
+
+    -> In progress ... <-
+    * TODO: Try to avoid calling the database three times
+        * you could theoretically call the database once on the server.js file
+        * and import the database const from other files
+        * 
+        * const database = new Database // server.js
+        * import database from '../server.js' // auth.js
 */
 
 // Passport config
