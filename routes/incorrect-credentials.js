b0c674fe7b9d9fb0cc0eb0e2700b273de2d55e2f
= <=>
2020-07-26 13:01:36 -0300

    Implemented the css

--- routes/incorrect-credentials.js
+++ routes/incorrect-credentials.js
@@ -1,7 +1,9 @@
+import auth from '../utils/auth.js'
+
 import express from 'express'
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', auth.checkNotAuthenticated, (req, res) => {
     res.render('incorrect-credentials.ejs')
 })
 
